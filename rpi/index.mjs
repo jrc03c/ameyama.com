e66bc29576c4ef8eaaae0fc262ca5ff168cbe1e6
jrc03c <jrc03c@pm.me>
2025-08-03 12:52:09 -0500

    ...

--- rpi/index.mjs
+++ rpi/index.mjs
@@ -1,11 +1,16 @@
 import { install as installStatsSubmodule } from "./submodules/stats/index.mjs"
 import { install as installMusicAppSubmodule } from "./submodules/music/back-end/index.mjs"
 import express from "express"
+import process from "node:process"
+
+if (!process.env.MUSIC_DIR) {
+  throw new Error("The environment variable `MUSIC_DIR` is undefined!")
+}
 
 const app = express()
 
 installStatsSubmodule(app)
-installMusicAppSubmodule(app, "/music")
+installMusicAppSubmodule(app, "/music", process.env.MUSIC_DIR)
 
 app.listen(3000, () => {
   console.log("Listening on port 3000...")
