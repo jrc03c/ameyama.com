75c17ff1186c4a43e42545d41362299a6984124c
jrc03c <jrc03c@pm.me>
2025-08-03 12:45:40 -0500

    ...

--- rpi/index.mjs
+++ rpi/index.mjs
@@ -1,9 +1,11 @@
 import { install as installStatsSubmodule } from "./submodules/stats/index.mjs"
+import { install as installMusicAppSubmodule } from "./submodules/music/back-end/index.mjs"
 import express from "express"
 
 const app = express()
 
 installStatsSubmodule(app)
+installMusicAppSubmodule(app, "/music")
 
 app.listen(3000, () => {
   console.log("Listening on port 3000...")
