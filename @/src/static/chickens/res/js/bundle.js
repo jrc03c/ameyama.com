045ed2cd5b6c999cbb1d2ba12cf3de44bb721757
jrc03c <jrc03c@pm.me>
2025-08-10 12:11:20 -0500

    ...

--- @/src/static/chickens/res/js/bundle.js
+++ @/src/static/chickens/res/js/bundle.js
@@ -4035,14 +4035,14 @@
           this.x = parentRect.x + parentRect.width / 2 - width / 2;
           this.y = parentRect.y + parentRect.height / 2 - height / 2;
           this.touchOffset.x = -this.width / 2;
-          this.touchOffset.y = -this.height * 1.25;
+          this.touchOffset.y = -this.height * 1.5;
         } else {
           this.height = parentRect.height;
           this.width = Math.round(this.height / 2);
           this.x = parentRect.x + parentRect.width / 2 - width / 2;
           this.y = parentRect.y;
           this.touchOffset.x = -this.width / 2;
-          this.touchOffset.y = -this.height * 1.25;
+          this.touchOffset.y = -this.height * 1.5;
         }
       },
       recomputeStyle() {
