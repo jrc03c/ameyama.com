b9e6a3e13f7073c6e941ec8104450269bd381e3f
jrc03c <jrc03c@pm.me>
2025-09-14 09:29:52 -0500

    ...

---
+++ @/serve.mjs
@@ -0,0 +1,26 @@
+import * as fsx from "@jrc03c/fs-extras"
+import express from "express"
+import path from "node:path"
+
+!(async () => {
+  const app = express()
+
+  const scripts = fsx.findSync(path.join(import.meta.dirname, "api"), f =>
+    f.endsWith(".mjs"),
+  )
+
+  for (const script of scripts) {
+    const handler = (await import(script)).default
+
+    app.use(
+      script.replace(import.meta.dirname, "").replace(/\.mjs$/g, ""),
+      handler,
+    )
+  }
+
+  app.use("/", express.static(import.meta.dirname, { extensions: ["html"] }))
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000...")
+  })
+})()
